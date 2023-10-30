af24ca70a72860c839e6afc5ad97fe97d96186a2
agus-mb <munosagustina3@gmail.com>
2023-10-30 20:45:45 -0300

    funciones basicas y movimiento de suelo

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
-document.addEventListener('DOMContentLoaded',function(){
-    console.log("Inicia juego");
-    var a1=new Area(0,30,100,100);
-    var a2=new Area(100,30,100,100);
-    medidas.tomaMedida();
-    medidas.dibujarTiles();
-    teclas.iniciar();
-});
+/*inicializacion de las variables basicas*/
 
-var start=document.getElementById("start");
-start.addEventListener('click',function(){
-    bucleInfinito.bucle();
-});
-var start=document.getElementById("mostrar");
-start.addEventListener('click',function(){
-    teclas.mostrarTeclas();
-});
-window.addEventListener('resize',function () {
-    medidas.dibujarTiles();
-});
+var time= new Date();
+var deltaTime=0;
 
-
-////////////////////////////
-
-var teclas={
-    arregloTeclas:new Array(),
-    iniciar:function () {
-        document.onkeydown=teclas.guardaTeclas;
-    },
-    guardaTeclas:function (e) {
-        teclas.arregloTeclas.push(e.key);
-        console.log(e.key);
-    },
-    teclaPulsada:function (codLetra){
-        return (teclas.arregloTeclas.indexOf(codLetra))?true:false;
-    },
-    borrarTeclas:function () {
-        teclas.arregloTeclas=new Array();
-    },
-    mostrarTeclas:function () {
-        console.log(teclas.arregloTeclas);
-    }
+if(document.readyState==="complete"||document.readyState==="interactive"){
+    setTimeout(Init,1);
+}else{
+    document.addEventListener('DOMContentLoaded',Init)
 }
+
+function Init(){
+    time=new Date();
+    Start();
+    Loop();
+
+}
+
+function Loop(){
+    deltaTime=(new Date()-time)/1000;
+    time= new Date();
+    Update();
+    requestAnimationFrame(Loop)
+}
+
+
+
+var sueloY = 22;
+var velY = 0;
+var impulso = 900;
+var gravedad = 2500;
+var dinoPosX = 42;
+var dinoPosY = sueloY;
+var sueloX = 0;
+var velEscenario = 1280 / 3;
+var gameVel = 1;
+var score = 0;
+var parado = false;
+var saltando = false;
+
+var contenedor;
+var dino;
+var textScore;
+var suelo;
+
+function Start() {
+  contenedor = document.querySelector(".contenedor");
+  dino = document.querySelector(".dino");
+  suelo = document.querySelector(".suelo");
+  textScore = document.querySelector(".score");
+
+}
+
+function Update() {
+    MoverSuelo();
+}
+
+function MoverSuelo() {
+    sueloX += CalcularDesplazamiento();
+    suelo.style.left = -(sueloX % contenedor.clientWidth) + "px";
+}
+
+function CalcularDesplazamiento() {
+    return velEscenario * deltaTime * gameVel;
+}
+
+
